37a5b36e3b035309aa6f9276938c69a681bda570
edupoly <info.edupoly@gmail.com>
2025-11-17 08:46:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-danger">Hello Edupoly</h1>
+      <h1 className="bg-success">Hello Meghana</h1>
+    </div>
+  );
+}
+
+export default App;
