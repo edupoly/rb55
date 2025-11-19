65355cf2381c6fbb5b0a9d2cec5451eb673b146a
edupoly <info.edupoly@gmail.com>
2025-11-19 08:46:44 +0530

    gallary

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Gallery from "./Gallery";
 
 function App() {
   return (
-    <div>
+    <div className="border border-2 m-2 p-2">
       <h1 className="text-danger">Hello Edupoly</h1>
-      <h1 className="bg-success">Hello Meghana</h1>
+      <Gallery></Gallery>
     </div>
   );
 }
