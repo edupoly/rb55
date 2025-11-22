8c569b085d3f0516cd176b54dde0a08fe3dec228
edupoly <info.edupoly@gmail.com>
2025-11-22 08:51:17 +0530

    performance-hooks

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Todo({ t, deleteTodo, i }) {
+  useEffect(() => {
+    console.log(`${t} rendered`);
+  });
   return (
-    <div className="border p-2 m-2">
-      <li>
+    <div className="border m-2">
+      <li className="d-flex gap-4 bg-success-subtle p-2">
         {t}
         <button
           onClick={() => {
